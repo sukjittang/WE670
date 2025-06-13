76d28dd0d411b449ea6ea87e61d6b9ca0403e28d
Sukjit Tangcharoen <67230096@dpu.ac.th>
2025-06-13 17:54:40 +0700

    edit

---
+++ 01/readwritefile.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+console.log(textIn);
+const textOut = `Avocado information: ${textIn}\n Create on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File written');
